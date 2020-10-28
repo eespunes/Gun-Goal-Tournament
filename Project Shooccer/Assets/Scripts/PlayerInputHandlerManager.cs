0f2fab0a98560e2c31a96aca1e43f020dad325fc
Erik Espuñes Juberó <erik.espunyes7@outlook.com>
2020-10-28 20:31:11 +0100

    Almost Finished

---
+++ Project Shooccer/Assets/Scripts/PlayerInputHandlerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandlerManager : MonoBehaviour
+{
+    private PlayerInputManager _playerInputManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _playerInputManager = GetComponent<PlayerInputManager>();
+        if (!MatchController.GetInstance().SplitScreen)
+        {
+            _playerInputManager.EnableJoining();
+            _playerInputManager.JoinPlayer();
+            _playerInputManager.JoinPlayer();
+        }
+        else _playerInputManager.DisableJoining();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
